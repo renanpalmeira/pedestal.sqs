0b94cced290547d19a6e2fdbc2d385a912754ab9
Renan Palmeira <renanpalmeira1@hotmail.com>
2019-07-22 00:43:07 -0300

    Update to 1.0.0-alpha8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pedestal.sqs "1.0.0-alpha7"
+(defproject pedestal.sqs "1.0.0-alpha8"
   :description "Pedestal interface for AWS SQS."
   :url "https://github.com/renanpalmeira/pedestal.sqs"
   :license {:name "The MIT License"
